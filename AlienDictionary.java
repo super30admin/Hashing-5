482fb275f0fa7da7fcdabc1ab30c27fb3f87f890
rahul-gajwani <rahul.gajwani10@gmail.com>
2022-09-03 20:15:24 +0530

    Create AlienDictionary.java

---
+++ AlienDictionary.java
@@ -0,0 +1,81 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+//TC: O(n.k); where n is number of words and k is avg length of a word.
+//SC: O(1) since map and queue will at most have 26 chars
+public class AlienDictionary { 
+	/**Approach: Graph + BFS**/
+	HashMap<Character, HashSet<Character>> map;
+    int[] inDegrees;
+    public String alienOrder(String[] words) {
+        map = new HashMap<>();
+        inDegrees = new int[26];      
+        buildGraph(words);           
+        //BFS
+        //Add all the independent nodes originally to queue to start.
+        Queue<Character> q= new LinkedList<>(); 
+        StringBuilder sb = new StringBuilder();
+        for(Character key: map.keySet()){ //O(1)
+            if(inDegrees[key - 'a'] == 0){
+                q.add(key);
+            }
+        }
+     
+        if(q.isEmpty()) return "";
+        //Else Process the queue
+        while(!q.isEmpty()){ 
+            char c = q.poll();
+            sb.append(c);
+            //Reduce indegrees of its dependents
+            for(char edge: map.get(c)){
+                inDegrees[edge - 'a']--;
+               
+                if(inDegrees[edge - 'a'] == 0){
+                    q.add(edge);
+                }
+            }
+        }
+        if(sb.length() != map.size()) return "";//All chars not processed.     
+        return sb.toString();
+    }
+    private void buildGraph(String[] words){
+        
+        for(String word: words){ //O(n)
+            for(char c: word.toCharArray()){
+                map.putIfAbsent(c, new HashSet<>());
+            }            
+        }
+        //Fill adjacency map and inDegrees
+        for(int i=0; i<words.length -1; i++){ //O(n)
+            String first = words[i]; int fl = first.length();
+            String second = words[i+1]; int sl = second.length();             
+          
+            if(fl!=sl && first.startsWith(second)) {
+                map = new HashMap();
+                break;
+            } 
+            //else fill the map and array
+            for(int j=0; j<fl && j<sl; j++){ //O(k)
+                char fChar = first.charAt(j);
+                char sChar = second.charAt(j);
+                if(fChar != sChar){
+                    Set<Character> set = map.get(fChar);                    
+                    if(!set.contains(sChar)) inDegrees[sChar - 'a']++;   
+                    set.add(sChar);
+                    break;
+                }                
+            }
+        }        
+    }
+
+	/** Driver code to test above **/
+	public static void main (String[] args) {	
+		AlienDictionary ob  = new AlienDictionary();	
+		String[] words = {"apple","app"};//{"wrt","wrf","er","ett","rftt"};
+
+		System.out.println("Sorted unique letters in alien language: "+ ob.alienOrder(words));         
+	}	
+}
